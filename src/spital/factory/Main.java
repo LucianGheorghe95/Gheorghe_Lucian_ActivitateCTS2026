267b71791818bbbb1ae2e69fad5bd88fadde744b
LucianGheorghe95 <gheorghe.lucian1995@yahoo.com>
2026-03-10 21:37:31 +0200

    Clasa PersonalFactory + apelarea in main

--- src/spital/factory/Main.java
+++ src/spital/factory/Main.java
@@ -1,4 +1,16 @@
 package spital.factory;
 
 public class Main {
+    public static void main(String[] args) {
+
+        PersonalFactory fabrica = new PersonalFactory();
+
+        PersonalSpital brancardier = fabrica.createInstance(TipPersonal.BRANCARDIER);
+        PersonalSpital asistent = fabrica.createInstance(TipPersonal.ASISTENT);
+        PersonalSpital medic = fabrica.createInstance(TipPersonal.MEDIC);
+
+        brancardier.descriere();
+        asistent.descriere();
+        medic.descriere();
+    }
 }
--- src/spital/factory/PersonalFactory.java
+++ src/spital/factory/PersonalFactory.java
@@ -1,4 +1,17 @@
 package spital.factory;
 
 public class PersonalFactory {
+
+    public PersonalSpital createInstance(TipPersonal tipPersonal) {
+        switch (tipPersonal) {
+            case BRANCARDIER:
+                return new Brancardier();
+            case ASISTENT:
+                return new Asistent();
+            case MEDIC:
+                return new Medic();
+            default:
+                return null;
+        }
+    }
 }
