82765712d805d7f2f6ae3290c6421616d1913349
LucianGheorghe95 <gheorghe.lucian1995@yahoo.com>
2026-03-17 21:52:25 +0200

    apelarea in main

--- src/spital/abstractfactory/Main.java
+++ src/spital/abstractfactory/Main.java
@@ -1,4 +1,22 @@
 package spital.abstractfactory;
 
 public class Main {
+    public static void main(String[] args) {
+
+        FactoryPersonalMedical factoryMedical = new FactoryPersonalMedical();
+        FactoryPersonalNonMedical factoryNonMedical = new FactoryPersonalNonMedical();
+
+        PersonalSpital medic = factoryMedical.createPersonal(TipPersonalMedical.MEDIC);
+        PersonalSpital asistent = factoryMedical.createPersonal(TipPersonalMedical.ASISTENT);
+        PersonalSpital brancardier = factoryMedical.createPersonal(TipPersonalMedical.BRANCARDIER);
+
+        PersonalSpital secretar = factoryNonMedical.createPersonal(TipPersonalNonMedical.SECRETAR);
+        PersonalSpital registrator = factoryNonMedical.createPersonal(TipPersonalNonMedical.REGISTRATOR);
+
+        medic.afisareDescriere();
+        asistent.afisareDescriere();
+        brancardier.afisareDescriere();
+        secretar.afisareDescriere();
+        registrator.afisareDescriere();
+    }
 }
