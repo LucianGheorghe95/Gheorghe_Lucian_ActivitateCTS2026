2372d7b8d920dc3a6df1c5e21b191e6dd9db2886
LucianGheorghe95 <gheorghe.lucian1995@yahoo.com>
2026-03-19 21:45:01 +0200

    apelare in main a design pattern prototype

--- src/prototype/Main.java
+++ src/prototype/Main.java
@@ -1,4 +1,17 @@
 package prototype;
 
+
+
 public class Main {
+    public static void main(String[] args) {
+
+        RetetaAbstracta retetaOriginala = new RetetaMedicament("Paracetamol", 10.5f, 5.0f, 2.5f);
+
+        RetetaAbstracta retetaCopie1 = retetaOriginala.cloneaza();
+        RetetaAbstracta retetaCopie2 = retetaOriginala.cloneaza();
+
+        System.out.println(retetaOriginala);
+        System.out.println(retetaCopie1);
+        System.out.println(retetaCopie2);
+    }
 }
