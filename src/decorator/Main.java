1901ba1363db5a5bc3f3754bbf2d17372c8d7b55
LucianGheorghe95 <gheorghe.lucian1995@yahoo.com>
2026-03-31 22:50:05 +0300

    main decorator

--- src/decorator/Main.java
+++ src/decorator/Main.java
@@ -1,4 +1,18 @@
 package decorator;
 
 public class Main {
+    public static void main(String[] args) {
+
+        RezultatAnalize rezultatInitial = new RezultatPrintat("Popescu Ion", "Analize bune");
+        rezultatInitial.afiseazaRezultat();
+
+        System.out.println();
+
+        RezultatAnalize rezultatCuOnline = new RezultatOnline(rezultatInitial);
+        rezultatCuOnline.afiseazaRezultat();
+
+        System.out.println();
+
+        rezultatInitial.afiseazaRezultat();
+    }
 }
